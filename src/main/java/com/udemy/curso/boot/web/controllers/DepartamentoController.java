e8c069a7b4526112e61ca94cde87ccfb6947f253
Filipe <filipeandrade000@gmail.com>
2023-02-03 06:40:43 -0300

    abrindo links

---
+++ src/main/java/com/udemy/curso/boot/web/controllers/DepartamentoController.java
@@ -0,0 +1,21 @@
+package com.udemy.curso.boot.web.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/departamentos")
+public class DepartamentoController {
+
+	@GetMapping("/cadastrar")
+	public String cadastrar() {
+		return "/departamento/cadastro";
+	}
+	
+	@GetMapping("/listar")
+	public String listar() {
+		return "/departamento/lista";
+	}
+	
+}
--- src/main/java/com/udemy/curso/boot/web/controllers/HomeController.java
+++ src/main/java/com/udemy/curso/boot/web/controllers/HomeController.java
@@ -1,4 +1,4 @@
-package com.udemy.curso.boot.web.controller;
+package com.udemy.curso.boot.web.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,6 +8,6 @@ public class HomeController {
 
 	@GetMapping("/")
 	public String home() {
-		return "home";
+		return "/home";
 	}
 }
