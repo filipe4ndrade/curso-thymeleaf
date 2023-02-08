ed60c7b230488207dbea8a72e78ff13771f5d0e3
Filipe <filipeandrade000@gmail.com>
2023-02-08 07:35:19 -0300

    funcionario

--- src/main/java/com/udemy/curso/boot/domain/Cargo.java
+++ src/main/java/com/udemy/curso/boot/domain/Cargo.java
@@ -1,11 +1,15 @@
 package com.udemy.curso.boot.domain;
 
+import java.util.List;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
+@SuppressWarnings("serial")
 @Entity
 @Table(name = "CARGOS")
 public class Cargo extends AbstractEntity<Long> {
@@ -17,6 +21,9 @@ public class Cargo extends AbstractEntity<Long> {
 	@JoinColumn(name = "id_departamento_fk")
 	private Departamento departamento;
 
+	@OneToMany(mappedBy = "cargo")
+	private List<Funcionario> funcionarios;
+	
 	public String getNome() {
 		return nome;
 	}
@@ -31,6 +38,14 @@ public class Cargo extends AbstractEntity<Long> {
 
 	public void setDepartamento(Departamento departamento) {
 		this.departamento = departamento;
+	}
+
+	public List<Funcionario> getFuncionarios() {
+		return funcionarios;
+	}
+
+	public void setFuncionarios(List<Funcionario> funcionarios) {
+		this.funcionarios = funcionarios;
 	}
 	
 	
--- src/main/java/com/udemy/curso/boot/domain/Departamento.java
+++ src/main/java/com/udemy/curso/boot/domain/Departamento.java
@@ -7,6 +7,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
+@SuppressWarnings("serial")
 @Entity
 @Table(name = "DEPARTAMENTOS")
 public class Departamento extends AbstractEntity<Long>{
