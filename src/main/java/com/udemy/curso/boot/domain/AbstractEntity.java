71628be26d3447711532e62cddc7f31d99279721
Filipe <filipeandrade000@gmail.com>
2023-02-08 07:04:03 -0300

    superclasse abstrata jpa

---
+++ src/main/java/com/udemy/curso/boot/domain/AbstractEntity.java
@@ -0,0 +1,51 @@
+package com.udemy.curso.boot.domain;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+
+@SuppressWarnings("serial")
+@MappedSuperclass
+public abstract class AbstractEntity<ID extends Serializable> implements Serializable {
+
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private ID id;
+
+	public ID getId() {
+		return id;
+	}
+
+	public void setId(ID id) {
+		this.id = id;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		AbstractEntity<?> other = (AbstractEntity<?>) obj;
+		return Objects.equals(id, other.id);
+	}
+
+	@Override
+	public String toString() {
+		return "id = " + id;
+	}
+	
+	
+}
