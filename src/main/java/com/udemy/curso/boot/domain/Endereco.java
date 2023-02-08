498c2e2c4d13afb53268bdcf9308170bc6d45c4e
Filipe <filipeandrade000@gmail.com>
2023-02-08 07:23:48 -0300

    endereco

---
+++ src/main/java/com/udemy/curso/boot/domain/Endereco.java
@@ -0,0 +1,91 @@
+package com.udemy.curso.boot.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.Table;
+
+@SuppressWarnings("serial")
+@Entity
+@Table(name = "ENDERECOS")
+public class Endereco extends AbstractEntity<Long> {
+
+	@Column(nullable = false)
+	private String logadouro;
+	
+	@Column(nullable = false)
+	private String bairro;
+	
+	@Column(nullable = false)
+	private String cidade;
+	
+	@Column(nullable = false, length = 2)
+	@Enumerated(EnumType.STRING)
+	private UF uf;
+	
+	@Column(nullable = false, length = 9)
+	private String cep;
+	
+	@Column(nullable = false, length = 5)
+	private Integer numero;
+	
+	private String complemento;
+
+	public String getLogadouro() {
+		return logadouro;
+	}
+
+	public void setLogadouro(String logadouro) {
+		this.logadouro = logadouro;
+	}
+
+	public String getBairro() {
+		return bairro;
+	}
+
+	public void setBairro(String bairro) {
+		this.bairro = bairro;
+	}
+
+	public String getCidade() {
+		return cidade;
+	}
+
+	public void setCidade(String cidade) {
+		this.cidade = cidade;
+	}
+
+	public UF getUf() {
+		return uf;
+	}
+
+	public void setUf(UF uf) {
+		this.uf = uf;
+	}
+
+	public String getCep() {
+		return cep;
+	}
+
+	public void setCep(String cep) {
+		this.cep = cep;
+	}
+
+	public Integer getNumero() {
+		return numero;
+	}
+
+	public void setNumero(Integer numero) {
+		this.numero = numero;
+	}
+
+	public String getComplemento() {
+		return complemento;
+	}
+
+	public void setComplemento(String complemento) {
+		this.complemento = complemento;
+	}
+	
+}
