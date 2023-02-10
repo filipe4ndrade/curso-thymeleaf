c31c480c105b455bd39be7687a29a4a6312cf66b
Filipe <filipeandrade000@gmail.com>
2023-02-10 03:17:49 -0300

    Controller e View, listando, editando e excluindo cargos parte 1

--- src/main/java/com/udemy/curso/boot/web/controllers/CargoController.java
+++ src/main/java/com/udemy/curso/boot/web/controllers/CargoController.java
@@ -1,20 +1,53 @@
 package com.udemy.curso.boot.web.controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.udemy.curso.boot.domain.Cargo;
+import com.udemy.curso.boot.domain.Departamento;
+import com.udemy.curso.boot.servicies.CargoService;
+import com.udemy.curso.boot.servicies.DepartamentoService;
 
 @Controller
 @RequestMapping("/cargos")
 public class CargoController {
 
+	
+	@Autowired
+	private CargoService cargoService;
+	@Autowired
+	private DepartamentoService departamentoService;
+	
 	@GetMapping("/cadastrar")
-	public String cadastrar() {
+	public String cadastrar(Cargo cargo) {
 		return "/cargo/cadastro";
 	}
 	
 	@GetMapping("/listar")
-	public String listar() {
+	public String listar(ModelMap model) {
+		model.addAttribute("cargos", cargoService.buscarTodos());
 		return "/cargo/lista";
 	}
+	
+	@PostMapping("/salvar")
+	public String salvar(Cargo cargo, RedirectAttributes attr) {
+		cargoService.salvar(cargo);
+		attr.addFlashAttribute("success", "Cargo inserido com sucesso.");
+		return "redirect:/cargos/cadastrar";
+	}
+	
+	@ModelAttribute("departamentos")
+	public List<Departamento> listaDeDepartamentos(){
+		return departamentoService.buscarTodos();
+	}
+	
+	
 }
---
+++ src/main/java/com/udemy/curso/boot/web/conversor/StringToCargoConversor.java
@@ -0,0 +1,24 @@
+package com.udemy.curso.boot.web.conversor;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.stereotype.Component;
+
+import com.udemy.curso.boot.domain.Cargo;
+import com.udemy.curso.boot.servicies.CargoService;
+
+@Component
+public class StringToCargoConversor implements Converter<String, Cargo> {
+
+	@Autowired
+	private CargoService service;
+	
+	@Override
+	public Cargo convert(String text) {
+		if (text.isEmpty()) {
+			return null;
+		}
+		Long id = Long.valueOf(text);
+		return service.buscarPorId(id);
+	}
+}
