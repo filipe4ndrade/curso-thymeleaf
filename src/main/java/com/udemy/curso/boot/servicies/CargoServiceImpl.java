13a7b3e7c06088dc87b53546ec1a86907cb35fa9
Filipe <filipeandrade000@gmail.com>
2023-02-10 05:33:17 -0300

    Controller e View, listando, editando e excluindo cargos parte 2

--- src/main/java/com/udemy/curso/boot/servicies/CargoServiceImpl.java
+++ src/main/java/com/udemy/curso/boot/servicies/CargoServiceImpl.java
@@ -42,4 +42,11 @@ public class CargoServiceImpl implements CargoService {
 		return dao.findAll();
 	}
 
+	@Override
+	public boolean cargoTemFuncionarios(Long id) {
+		if (buscarPorId(id).getFuncionarios().isEmpty()) {
+			return false;
+		}
+		return true;
+	}
 }
--- src/main/java/com/udemy/curso/boot/web/controllers/CargoController.java
+++ src/main/java/com/udemy/curso/boot/web/controllers/CargoController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -19,13 +20,12 @@ import com.udemy.curso.boot.servicies.DepartamentoService;
 @Controller
 @RequestMapping("/cargos")
 public class CargoController {
-
 	
 	@Autowired
 	private CargoService cargoService;
 	@Autowired
 	private DepartamentoService departamentoService;
-	
+
 	@GetMapping("/cadastrar")
 	public String cadastrar(Cargo cargo) {
 		return "/cargo/cadastro";
@@ -34,7 +34,7 @@ public class CargoController {
 	@GetMapping("/listar")
 	public String listar(ModelMap model) {
 		model.addAttribute("cargos", cargoService.buscarTodos());
-		return "/cargo/lista";
+		return "/cargo/lista"; 
 	}
 	
 	@PostMapping("/salvar")
@@ -44,10 +44,32 @@ public class CargoController {
 		return "redirect:/cargos/cadastrar";
 	}
 	
-	@ModelAttribute("departamentos")
-	public List<Departamento> listaDeDepartamentos(){
-		return departamentoService.buscarTodos();
+	@GetMapping("/editar/{id}")
+	public String preEditar(@PathVariable("id") Long id, ModelMap model) {
+		model.addAttribute("cargo", cargoService.buscarPorId(id));
+		return "cargo/cadastro";
 	}
 	
+	@PostMapping("/editar")
+	public String editar(Cargo cargo, RedirectAttributes attr) {
+		cargoService.editar(cargo);
+		attr.addFlashAttribute("success", "Registro atualizado com sucesso.");
+		return "redirect:/cargos/cadastrar";
+	}
 	
+	@GetMapping("/excluir/{id}")
+	public String excluir(@PathVariable("id") Long id, RedirectAttributes attr) {
+		if (cargoService.cargoTemFuncionarios(id)) {
+			attr.addFlashAttribute("fail", "Cargo não excluido. Tem funcionário(s) vinculado(s).");
+		} else {
+			cargoService.excluir(id);
+			attr.addFlashAttribute("success", "Cargo excluido com sucesso.");
+		}
+		return "redirect:/cargos/listar";
+	}
+	
+	@ModelAttribute("departamentos")
+	public List<Departamento> listaDeDepartamentos() {
+		return departamentoService.buscarTodos();
+	}	
 }
